21e26f6121b425eefb61ae1738a5fb79e4dbfa43
Darmohrai <darmovad.com>
2024-02-17 16:54:43 +0200

    create class Employee

---
+++ Employee.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 17.02.2024.
+//
+
+#include "Employee.h"
---
+++ Employee.h
@@ -0,0 +1,14 @@
+//
+// Created by HP on 17.02.2024.
+//
+
+#ifndef PAYMENT_SYSTEM_OOP_EMPLOYEE_H
+#define PAYMENT_SYSTEM_OOP_EMPLOYEE_H
+
+
+class Employee {
+
+};
+
+
+#endif //PAYMENT_SYSTEM_OOP_EMPLOYEE_H
