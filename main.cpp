6354f5f7cb5bf77004edbe77366f67df4a7e9c9b
Darmohrai <darmovad.com>
2024-02-17 20:53:25 +0200

    add input of information in class Employee

--- main.cpp
+++ main.cpp
@@ -2,7 +2,32 @@
 #include "Employee.h"
 
 int main() {
-    Employee emp;
-    emp.getInfo();
+    int n;
+    std::cout << "enter number of employee: ";
+    std::cin >> n;
+
+    Employee* worker = new Employee[n];
+
+    std::string full_name, position;
+    int salary, age;
+
+    for (int i = 0; i < n; i++){
+        std::cout << "Enter the worker's name: ";
+        std::cin >> full_name;
+
+        std::cout << "Enter the worker's position: ";
+        std::cin >> position;
+
+        std::cout << "Enter the worker's age: ";
+        std::cin >> age;
+
+        std::cout << "Enter the worker's salary: ";
+        std::cin >> salary;
+
+        worker[i] = Employee(full_name, position, age, salary);
+
+        worker[i].getInfo();
+    }
+
     return 0;
 }
