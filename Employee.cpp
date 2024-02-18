59f2a43bbc95a2b5b620337c291823e9366c09d1
Darmohrai <darmovad.com>
2024-02-18 23:43:35 +0200

    move constructors from Employee.h to Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -4,6 +4,34 @@
 
 #include "Employee.h"
 
+Employee::Employee(int i){
+    std::cout << std::endl << "Enter the worker's name: ";
+    std::cin >> this->full_name;
+    std::cout << "Enter the worker's position: ";
+    std::cin >> this-> position;
+    std::cout << "Enter the worker's age: ";
+    std::cin >> this->age;
+    std::cout << "Enter the worker's salary: ";
+    std::cin >> this->salary;
+}
+
+Employee::Employee(int salary, std::string &full_name, int age){
+    this->full_name = full_name;
+    this->age = age;
+
+    std::cout << "write new position: ";
+    std::cin >> this->position;
+    std::cout << "write new salary: ";
+    std::cin >> this->salary;
+
+    int difference = this->salary - salary;
+    std::cout << "salary rise on " << difference;
+
+    if (difference > 0){
+        std::cout << std::endl << this->full_name << " has promotion " << std::endl;
+    }
+}
+
 void Employee::getInfo(){
     std::cout << std::endl << "full name: " << full_name << std::endl
               << "position: " << position << std::endl
--- Employee.h
+++ Employee.h
@@ -18,36 +18,9 @@ private:
 public:
     Employee() : full_name("No_Info"), position("No_Info"), age(0), salary(0) {}
 
-    Employee(int i){
-        std::cout << std::endl << "Enter the worker's name: ";
-        std::cin >> this->full_name;
-        std::cout << "Enter the worker's position: ";
-        std::cin >> this-> position;
-        std::cout << "Enter the worker's age: ";
-        std::cin >> this->age;
-        std::cout << "Enter the worker's salary: ";
-        std::cin >> this->salary;
-    }
+    Employee(int i);
 
-    Employee(int salary, std::string &full_name, int age){
-        this->full_name = full_name;
-        this->age = age;
-
-        std::cout << "write new position: ";
-        std::cin >> this->position;
-        std::cout << "write new salary: ";
-        std::cin >> this->salary;
-
-        int difference = this->salary - salary;
-        std::cout << "salary rise on " << difference;
-
-        if (difference > 0){
-            std::cout << std::endl << this->full_name << " has promotion " << std::endl;
-        }
-        if (difference < 0){
-            std::cout << std::endl << this->full_name << " has demotion " << std::endl;
-        }
-    }
+    Employee(int salary, std::string &full_name, int age);
 
     void getInfo();
 
